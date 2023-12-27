6189b055cad328fb82c880c3fe5175c858108e5c
Muqimjon <muqimjon.mr@gmail.com>
2023-12-27 12:28:47 +0500

    🔒 singleton with function

---
+++ src/Creational/Sigleton.Console/SingletonWithFunction.cs
@@ -0,0 +1,16 @@
+﻿namespace Sigleton.Console;
+
+internal class SingletonWithFunction
+{
+    private static SingletonWithFunction? instance;
+
+    private SingletonWithFunction() { }
+
+    public static SingletonWithFunction GetInstance()
+    {
+        instance ??= new SingletonWithFunction();
+        return instance;
+    }
+
+    // other methods
+}
