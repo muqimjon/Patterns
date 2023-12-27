527abbb0f39eb012ccf5a420c96975ac1fdb6344
Muqimjon <muqimjon.mr@gmail.com>
2023-12-27 12:25:56 +0500

    🔒 Singleton with Property 1

---
+++ src/Creational/Sigleton.Console/SingletonWithProperty.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton.Console;
+
+internal class SingletonWithProperty1
+{
+    private SingletonWithProperty1() { }
+
+    private static SingletonWithProperty1? instance;
+    public static SingletonWithProperty1 Instance
+    {
+        get
+        {
+            instance ??= new SingletonWithProperty1();
+            return instance;
+        }
+    }
+}
