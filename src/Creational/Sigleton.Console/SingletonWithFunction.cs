247830aca1e1ee07605d346fcd9a439220b65b00
Muqimjon <muqimjon.mr@gmail.com>
2023-12-28 07:49:27 +0500

    🔒 singleton with functin 2

--- src/Creational/Sigleton.Console/SingletonWithFunction.cs
+++ src/Creational/Sigleton.Console/SingletonWithFunction.cs
@@ -1,16 +1,28 @@
 ﻿namespace Sigleton.Console;
 
-internal class SingletonWithFunction
+internal class SingletonWithFunction1
 {
-    private static SingletonWithFunction? instance;
+    private static SingletonWithFunction1? instance;
 
-    private SingletonWithFunction() { }
+    private SingletonWithFunction1() { }
 
-    public static SingletonWithFunction GetInstance()
+    public static SingletonWithFunction1 GetInstance()
     {
-        instance ??= new SingletonWithFunction();
+        instance ??= new SingletonWithFunction1();
         return instance;
     }
+
+    // other methods
+}
+
+internal class SingletonWithFunction2
+{
+    private static SingletonWithFunction2 instance = new();
+
+    private SingletonWithFunction2() { }
+
+    public static SingletonWithFunction2 GetInstance()
+        => instance;
 
     // other methods
 }
